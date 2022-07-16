71b8b3a68919a361c948350ef9e30dd0b4e6ab6c
minorunara <70518500+minorunara@users.noreply.github.com>
2022-07-17 01:08:41 +0900

    Add files via upload
    initial commit

---
+++ ToriiGates/Plugin.cs
@@ -0,0 +1,99 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using Bindito.Core;
+using Timberborn.AssetSystem;
+using Timberborn.SingletonSystem;
+using TimberbornAPI;
+using TimberbornAPI.AssetLoaderSystem.AssetSystem;
+using TimberbornAPI.Common;
+using UnityEngine;
+
+
+
+namespace TimberbornToriiGates
+{
+    [BepInPlugin(PluginGuid, "toriigates", PluginVersion)]
+    [BepInDependency("com.timberapi.timberapi")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string PluginGuid = "minorunara.Timberborn.ToriiGates";
+        public const string PluginVersion = "1.0.0";
+
+        public static ManualLogSource Log;
+
+        private void Awake()
+        {
+            Log = Logger;
+
+            TimberAPI.AssetRegistry.AddSceneAssets(PluginGuid, SceneEntryPoint.Global);
+
+            TimberAPI.DependencyRegistry.AddConfiguratorBeforeLoad(new BuildingAddConfigurator(), SceneEntryPoint.MainMenu);
+
+            Log.LogInfo($"Loaded {PluginGuid} {PluginVersion}!");
+        }
+    }
+
+    public class BuildingAdd : ILoadableSingleton
+    {
+        private readonly IAssetLoader _assetLoader;
+        private readonly IResourceAssetLoader _resourceAssetLoader;
+
+        public BuildingAdd(IAssetLoader assetLoader, IResourceAssetLoader resourceAssetLoader)
+        {
+            _assetLoader = assetLoader;
+            _resourceAssetLoader = resourceAssetLoader;
+        }
+
+        public void Load()
+        {
+            var platformModel = _resourceAssetLoader.Load<GameObject>("Buildings/Paths/Platform/Platform.Full.Folktails");
+            var shader = platformModel.GetComponent<MeshRenderer>().materials[0].shader;
+
+            AddBuilding("PrimitiveToriiGate", shader);  // 500
+            AddBuilding("SimpleToriiGate", shader);     // 501
+            AddBuilding("DemoToriiGate", shader);       // 502
+            AddBuilding("TreatedToriiGate", shader);    // 503
+            AddBuilding("IronToriiGate", shader);       // 504
+            AddBuilding("HugeToriiGateFT", shader);     // 505
+            AddBuilding("HugeToriiGateIT", shader);     // 506
+
+            Plugin.Log.LogInfo($"Loaded buildings!");
+        }
+
+        private void AddBuilding(string name, Shader shader)
+        {
+            var building = _assetLoader.Load<GameObject>(Plugin.PluginGuid, $"toriigates.bundle/{name}");
+
+            FixMaterialShader(building, shader);
+            TimberAPI.CustomObjectRegistry.AddGameObject(building);
+        }
+
+        private static void FixMaterialShader(GameObject obj, Shader shader)
+        {
+            var meshRenderer = obj.GetComponent<MeshRenderer>();
+            if (meshRenderer)
+            {
+                foreach (var mat in meshRenderer.materials)
+                {
+                    mat.shader = shader;
+                }
+            }
+
+            foreach (Transform child in obj.transform)
+            {
+                if (child.gameObject)
+                {
+                    FixMaterialShader(child.gameObject, shader);
+                }
+            }
+        }
+    }
+
+    public class BuildingAddConfigurator : IConfigurator
+    {
+        public void Configure(IContainerDefinition containerDefinition)
+        {
+            containerDefinition.Bind<BuildingAdd>().AsSingleton();
+        }
+    }
+}
---
+++ ToriiGates/ThunderstorePackage/README.md
@@ -0,0 +1,40 @@
+Hail to the Shinto Beavers!
+
+This mod adds 7 different torii gates to the decoration.
+
+## List of added decoration
+
+![ToriiGates](https://github.com/minorunara/TimberbornMods/ToriiGates/attachments/ToriiGates.png?raw=true)
+
+![DemoVerToriiGate](https://github.com/minorunara/TimberbornMods/ToriiGates/attachments/DemoVerToriiGate.png?raw=true)
+
+- Primitive Torii Gate
+- Simple Torii Gate
+- Demo ver. Torii Gate
+- Treated Torii Gate
+- Iron Torii Gate
+- Huge Torii Gate (FT)
+- Huge Torii Gate (IT)
+
+## Disclaimer!
+
+This mod is currently only supported in the **experimental** version (v0.2.1.0) of Timberborn.
+
+Other versions will probably not work (and may crash the game).
+
+## Install
+
+If you install manually, put the downloaded folder in the folder where Timberborn.exe is located \BepInEx\plugins\
+
+Remember that you will also need to install any mods that are dependencies.
+
+## How to use
+
+You should find the Torii Gates under the decorations tab.
+
+
+## Changelog
+
+### 1.0.0 Target game version is (v0.2.1.0) - 2022/07/16
+
+- Released
