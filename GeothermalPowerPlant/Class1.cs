bd053d889a2dae89f604cbe60488a8cf435ad28c
minorunara <70518500+minorunara@users.noreply.github.com>
2022-08-12 12:40:12 +0900

    1.0.0

---
+++ GeothermalPowerPlant/Class1.cs
@@ -0,0 +1,92 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using Bindito.Core;
+using Timberborn.AssetSystem;
+using Timberborn.SingletonSystem;
+using TimberbornAPI;
+using TimberbornAPI.AssetLoaderSystem.AssetSystem;
+using TimberbornAPI.Common;
+using UnityEngine;
+
+namespace TimberbornHotDryRockTurbine
+{
+
+    [BepInPlugin(PluginGuid, "GeothermalPowerPlant", PluginVersion)]
+    [BepInDependency("com.timberapi.timberapi")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string PluginGuid = "minorunara.Timberborn.GeothermalPowerPlant";
+        public const string PluginVersion = "1.0.0";
+
+        public static ManualLogSource Log;
+
+        private void Awake()
+        {
+            Log = Logger;
+
+            TimberAPI.AssetRegistry.AddSceneAssets(PluginGuid, SceneEntryPoint.Global);
+
+            TimberAPI.DependencyRegistry.AddConfiguratorBeforeLoad(new BuildingAddConfigurator(), SceneEntryPoint.MainMenu);
+
+            Log.LogInfo($"Loaded {PluginGuid} {PluginVersion}!");
+        }
+    }
+
+    public class BuildingAdd : ILoadableSingleton
+    {
+        private readonly IAssetLoader _assetLoader;
+        private readonly IResourceAssetLoader _resourceAssetLoader;
+
+        public BuildingAdd(IAssetLoader assetLoader, IResourceAssetLoader resourceAssetLoader)
+        {
+            _assetLoader = assetLoader;
+            _resourceAssetLoader = resourceAssetLoader;
+        }
+
+        public void Load()
+        {
+            var platformModel = _resourceAssetLoader.Load<GameObject>("Buildings/Paths/Platform/Platform.Full.Folktails");
+            var shader = platformModel.GetComponent<MeshRenderer>().materials[0].shader;
+
+            AddBuilding("GeothermalPowerPlant", shader);
+            AddBuilding("CoolingTower", shader);
+
+            Plugin.Log.LogInfo($"Loaded buildings!");
+        }
+
+        private void AddBuilding(string name, Shader shader)
+        {
+            var building = _assetLoader.Load<GameObject>(Plugin.PluginGuid, $"geothermalpowerplant.bundle/{name}");
+
+            FixMaterialShader(building, shader);
+        }
+
+        private static void FixMaterialShader(GameObject obj, Shader shader)
+        {
+            var meshRenderer = obj.GetComponent<MeshRenderer>();
+            if (meshRenderer)
+            {
+                foreach (var mat in meshRenderer.materials)
+                {
+                    mat.shader = shader;
+                }
+            }
+
+            foreach (Transform child in obj.transform)
+            {
+                if (child.gameObject)
+                {
+                    FixMaterialShader(child.gameObject, shader);
+                }
+            }
+        }
+    }
+
+    public class BuildingAddConfigurator : IConfigurator
+    {
+        public void Configure(IContainerDefinition containerDefinition)
+        {
+            containerDefinition.Bind<BuildingAdd>().AsSingleton();
+        }
+    }
+}
---
+++ GeothermalPowerPlant/ThunderstorePackage/README.md
@@ -0,0 +1,60 @@
+## Geothermal Power Plant
+
+Beaver civilization has now entered the machine age! Factories here and there are producing large quantities of goods, and the demand for energy is higher than ever. By the way, don't you have a surplus of underground ruins?
+
+This mod adds a geothermal power plant to be built on top of the underground ruins, and a cooling tower.
+
+It should bring a challenge to the end game.
+
+There are currently two facilities.
+### Hot Dry Rock Geothermal Turbine
+
+This facility consumes vast amounts of water to produce energy. It can be built only on top of underground ruins. Make sure there is enough water and a pump nearby. No water? Well then...I guess it's time to remind you that you are a beaver. By the way, the underground ruins were used by humans for deep geological disposal (headcanon), so beavers suffer from radiation damage. A sufficiently high level of happiness would offset this.
+
+![HotDryRockGeothermalTurbine](https://github.com/minorunara/TimberbornMods/blob/main/GeothermalPowerPlant/attachments/HotDryRockGeothermalTurbine.png?raw=true)
+
+### Cooling Tower
+
+This facility consumes metal blocks, treated plates and energy to produce water. It can only be built on top of a geothermal power plant (note the orientation).
+
+![CoolingTower](https://github.com/minorunara/TimberbornMods/blob/main/GeothermalPowerPlant/attachments/CoolingTower.png?raw=true)
+
+## Disclaimer!
+
+This mod is currently only supported in Timberborn version 0.2.4.0. Other versions will probably not work (and may crash the game).
+
+## Install
+
+If you install manually, put the downloaded folder in the folder where Timberborn.exe is located \BepInEx\plugins\
+
+Remember that you will also need to install any mods that are dependencies.
+
+## How to use
+
+You should find under the Power tab.
+
+## License
+
+This mod was made from Elec's ExtendedArchitecture, so the license follows the original mod.
+
+https://github.com/Timberborn-Modding-Central/ExtendedArchitecture/tree/main/Assets/ExtendedArchitecture
+
+## Known Issues
+
+Cooling towers can produce water even if the geothermal plant was not in operation.
+
+Trying to connect straight power shafts may cause them to change direction. Use otherwise.
+
+The metal texture is wrong.
+
+At certain points in a Hot Dry Rock Geothermal Turbine, something other than a cooling tower can be built. This is intentional to connect the cooling towers to the geothermal power plant.
+
+A bridge can be built through the chimney.
+
+Beavers can walk through the top of the Hot Dry Rock Geothermal Turbine.
+
+## Changelog
+
+### 1.0.0 Target game version is (0.2.4.0) - 2022/08/12
+
+Released
